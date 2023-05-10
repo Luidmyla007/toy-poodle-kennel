86a42ab13ffc4998f5ee546e55a9ff1f586bc533
Liudmyla007 <ldatsenko3@gmail.com>
2023-05-10 21:39:15 +0300

    add react router dom

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import { StyledLoader, GifLoader } from "./Loader.styled";
 export const Loader = () => {
   return (
     <StyledLoader>
-       <GifLoader  src="https://media.giphy.com/media/5VXc3ELd2VvZKQG9PX/giphy.gif"
+       <GifLoader src="https://media.giphy.com/media/5VXc3ELd2VvZKQG9PX/giphy.gif"
         alt="dog in love"></GifLoader>
     </StyledLoader>
   );
